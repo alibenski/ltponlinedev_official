b8a0f521af753c22c985ef1a97cb9390384c35bb
Allyson Frias <alifriassss@yahoo.com>
2018-10-21 21:36:52 +0200

    new user outside view text changes

--- resources/views/users_new/new_outside_user_form.blade.php
+++ resources/views/users_new/new_outside_user_form.blade.php
@@ -45,11 +45,11 @@
                             <div class="col-md-6">
                             <input name="contractfile" type="file" class="col-md-12 form-control-static" required="required">
                                 @if ($errors->has('contractfile'))
-                                    <span class="help-block">
+                                    <span class="alert alert-danger help-block">
                                         <strong>{{ $errors->first('contractfile') }}</strong>
                                     </span>
                                 @endif
-                                <p class="small text-danger"><strong>File size must less than 8MB</strong></p>
+                                <p class="small text-danger"><strong>File size must be less than 8MB</strong></p>
                             </div>
                         </div>
 
