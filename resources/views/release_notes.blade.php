68ee4b252a6faa8a49beba6b573e2f8522641cb2
Allyson Frias <allyson.frias@un.org>
2019-01-14 11:54:33 +0100

    update release notes

--- resources/views/release_notes.blade.php
+++ resources/views/release_notes.blade.php
@@ -1,39 +1,57 @@
-<h3>CLM Language Online Registration Release Notes</h3>
-<h4>Release Notes: 31 December 2018</h4>
+<style type="text/css">
 
-<li>sorting of waitlist - debugged orderBy priority and submission date</li>
-<li>waitlist count is not automatically updated - refresh manually</li>
-<li>classlist for teachers on 31 December</li>
-<li>pending payment students / late approval of payments inserted to waitlisted classes</li>
-<li>admin comments when moving students</li>
-<li>link stat counter to term in admin dashboard</li>
-<li>Moving of students directly in PASH Table</li>
-<li>Table showing Classroom count each</li>
+.tab { margin-left: 40px; }
+section { font-family: 'Source Sans Pro','Helvetica Neue',Helvetica,Arial,sans-serif;  }
+</style>
+<section>
+	
+	<h3>CLM Language Online Registration Release Notes</h3>
+	<h4>Release Notes: 31 December 2018</h4>
 
-<h4>Release Notes: 11 January 2019</h4>
+	<li>sorting of waitlist - debugged orderBy priority and submission date</li>
+	<li>waitlist count is not automatically updated - refresh manually</li>
+	<li>classlist for teachers on 31 December</li>
+	<li>pending payment students / late approval of payments inserted to waitlisted classes</li>
+	<li>admin comments when moving students</li>
+	<li>link stat counter to term in admin dashboard</li>
+	<li>Moving of students directly in PASH Table</li>
+	<li>Table showing Classroom count each</li>
+	
 
-<li>Copy 189 Term Results</li>
-<li>Quick edit of existing teachers</li>
-<li>Create teacher accounts and permissions</li>
-	Clean teachers table - update email and index no.
-<li>Create view of classes</li>
-<li>Create attendance tracking view</li>
+	<h4>Release Notes: 11 January 2019</h4>
+
+	<li>Copy 189 Term Results</li>
+	<li>Quick edit of existing teachers</li>
+	<li>Create teacher accounts and permissions</li>
+		<li class="tab">Clean teachers table - update email and index no.</li>
+	<li>Create view of classes</li>
+	<li>Create attendance tracking view</li>
+	<li>Create Release Notes page</li>
 
 
-<h4>Release Notes: 11 February 2019 - Preparation for 194 Enrollment</h4>
+	<h4>Release Notes: 11 February 2019 - Preparation for 194 Enrollment</h4>
 
-<li>Control next level of student</li>
-<li>Student can edit form and add attachments too</li>
-<li>Disapprove admin button/function for new user requests</li>
-<li>Update My Profile view to include the PROFILE field and DEPT field separately (case: Interns)</li>
-<li>Update SDDEXTR and User table when students update their profile</li>
-<li>Change of text in the enrolment forms and website as suggested by teachers (English)</li>
-<li>From Carol W, text on the placement form for English : "If you are in Geneva, please select one of the dates shown. If you are outside Geneva, please select the online option"</li>
-<li>Students form approval status field - 1 2 3 </li>
+	<li>Control next level of student</li>
+	<li>Student can edit form and add attachments too</li>
+	<li>Disapprove admin button/function for new user requests</li>
+	<li>Update My Profile view to include the PROFILE field and DEPT field separately (case: Interns)</li>
+	<li>Update SDDEXTR and User table when students update their profile</li>
+	<li>Change of text in the enrolment forms and website as suggested by teachers (English)</li>
+	<li>From Carol W, text on the placement form for English : "If you are in Geneva, please select one of the dates shown. If you are outside Geneva, please select the online option"</li>
+	<li>Students form approval status field for easy query - 1 2 3 </li>
+	<li>Check box disclaimer of manager approval instead of emailing the manager</li>
+		<li class="tab">Get text</li>
+		<li class="tab">Change process flow (code) - no email for UNOG staff</li>
+	<li>Comment box in enrolment form</li>
+	<li>Enter text explaining 1 form is for 1 language before submit button</li>
+	<li>Explanation of "flexible" button</li>
+		<li class="tab">Get text</li>
 
-<h4>Develoopment Freeze: 4-22 February 2019</h4>
+	<h4>Development Freeze: 4-22 February 2019</h4>
 
-<li>Development freeze on Online Enrolment Platform (Student Access and Views) - Hot fixes on issues only</li>
-<li>Teachers' view</li>
-	Results view but with P F I grades select
-	Assigning to next term course
+	<li>Development freeze on Online Enrolment Platform (Student Access and Views) - Hot fixes on issues only</li>
+	<li>Teachers' view</li>
+		<li class="tab">Results view but with P F I grades select</li>
+		<li class="tab">Assigning to next term course</li>
+	
+</section>
