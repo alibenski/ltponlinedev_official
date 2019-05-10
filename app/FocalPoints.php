bbf9421fb38e610d207bbc84779fb6b2664580f2
Allyson Frias <allyson.frias@un.org>
2019-05-10 11:44:26 +0200

    hotfix for deleted focalpoint still receiving - added SoftDeletes

--- app/FocalPoints.php
+++ app/FocalPoints.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class FocalPoints extends Model
 {
+		use SoftDeletes;
+
     protected $table = 'tblLTP_FocalPoints';
 
     public function torgan() {
