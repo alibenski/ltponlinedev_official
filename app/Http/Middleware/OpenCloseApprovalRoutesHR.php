37c7723d39892bb56e717204086b91fa02d4780c
Allyson Frias <allyson.frias@un.org>
2019-06-11 12:51:38 +0200

    catch error: payload invalid

--- app/Http/Middleware/OpenCloseApprovalRoutesHR.php
+++ app/Http/Middleware/OpenCloseApprovalRoutesHR.php
@@ -8,6 +8,7 @@ use Closure;
 use DB;
 use Illuminate\Support\Facades\Crypt;
 use Session;
+use Illuminate\Contracts\Encryption\DecryptException;
 
 class OpenCloseApprovalRoutesHR
 {
@@ -20,18 +21,22 @@ class OpenCloseApprovalRoutesHR
      */
     public function handle($request, Closure $next)
     {
-        // get current date
-        $now_date = Carbon::now();
-        // get the term from http route parameters via $request
-        $term = Crypt::decrypt($request->route('term'));
+        try {
+            // get current date
+            $now_date = Carbon::now();
+            // get the term from http route parameters via $request
+            $term = Crypt::decrypt($request->route('term'));
 
-        $approvalDateLimit = Term::where('Term_Code', $term)->value('Approval_Date_Limit_HR');
-        if ($approvalDateLimit == null || $now_date <= $approvalDateLimit) {
-            return $next($request);
+            $approvalDateLimit = Term::where('Term_Code', $term)->value('Approval_Date_Limit_HR');
+            if ($approvalDateLimit == null || $now_date <= $approvalDateLimit) {
+                return $next($request);
+            }
+
+            // redirect to page telling that the duration for approval has passed
+            // return redirect()->route('confirmationLinkExpired');
+            abort(403, 'Unauthorized action. Date limit for approval reached.');
+        } catch (DecryptException $e) {
+            abort(403, 'Error: Invalid Payload. Please click the link from the email message again.');
         }
-
-        // redirect to page telling that the duration for approval has passed
-        // return redirect()->route('confirmationLinkExpired');
-        abort(403, 'Unauthorized action. Date limit for approval reached.');
     }
 }
