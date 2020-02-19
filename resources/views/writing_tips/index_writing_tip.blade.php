f014603b2ae17bc2cdd52a5028164061c4a32450
Allyson Frias <allyson.frias@un.org>
2020-02-19 10:00:36 +0100

    apply dataTables to writing tips index table

--- resources/views/writing_tips/index_writing_tip.blade.php
+++ resources/views/writing_tips/index_writing_tip.blade.php
@@ -2,6 +2,7 @@
 
 @section('customcss')
 <link href="https://fonts.googleapis.com/css?family=Lobster&display=swap" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.10.18/af-2.3.3/b-1.5.6/b-colvis-1.5.6/b-flash-1.5.6/b-html5-1.5.6/b-print-1.5.6/cr-1.5.0/fc-3.2.5/fh-3.1.4/kt-2.5.0/r-2.2.2/rg-1.1.0/rr-1.2.4/sc-2.0.0/sl-1.3.0/datatables.min.css"/>
 @stop
 
 @section('content')
@@ -14,14 +15,15 @@
         <a href="{{ route('writing-tips.create') }}" class="btn btn-default"><i class="fa fa-pencil"></i> Create a new entry</a>
     </div>
 
-    <table class="table table-bordered table-striped" style="background-color: white;">
+    <table id="sampol" class="table table-bordered table-striped" style="background-color: white;">
+        <thead>
         <tr>
             <th>#</th>
             <th>Language</th>
             <th>Subject</th>
             <th>Operation</th>
         </tr>
-        
+        </thead>
         @foreach ($records as $record)
         <tr>
             <td>
@@ -45,7 +47,17 @@
 @stop
 
 @section('java_script')
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.10.18/af-2.3.3/b-1.5.6/b-colvis-1.5.6/b-flash-1.5.6/b-html5-1.5.6/b-print-1.5.6/cr-1.5.0/fc-3.2.5/fh-3.1.4/kt-2.5.0/r-2.2.2/rg-1.1.0/rr-1.2.4/sc-2.0.0/sl-1.3.0/datatables.min.js"></script>
 <script>
-
+$('#sampol').DataTable({
+    "fixedHeader": true,
+    "deferRender": true,
+    "dom": 'B<"clear">lfrtip',
+    "buttons": [
+            'copy', 'csv', 'excel', 'pdf'
+        ],
+});
 </script>
 @stop
