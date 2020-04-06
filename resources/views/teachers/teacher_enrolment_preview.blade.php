ee1da6a830807f8f9c611482e81dce993fb2cd31
Allyson Frias <allyson.frias@un.org>
2020-04-06 16:29:54 +0200

    Change ajax get to post - preview-get-student-count-per-class and preview-get-student-priority-status

--- resources/views/teachers/teacher_enrolment_preview.blade.php
+++ resources/views/teachers/teacher_enrolment_preview.blade.php
@@ -405,7 +405,7 @@ $(document).ready(function() {
 
 		$.ajax({
 			url: '{{ route('ajax-preview-get-student-priority-status') }}',
-			type: 'GET',
+			type: 'POST',
 			data: {arr:arr,eform_submit_count:eform_submit_count,L:L,term:term,_token:token},
 		})
 		.done(function(data) {
--- routes/web.php
+++ routes/web.php
@@ -164,8 +164,8 @@ Route::group(['middleware' => ['auth', 'isAdmin', 'first-time-login'], 'prefix' 
 
     Route::get('preview-merged-forms', 'PreviewController@previewMergedForms')->name('preview-merged-forms');
     Route::post('ajax-preview-course-boxes', ['as' => 'ajax-preview-course-boxes', 'uses' => 'PreviewController@ajaxPreviewCourseBoxes']);
-    Route::get('ajax-preview-get-student-count', ['as' => 'ajax-preview-get-student-count', 'uses' => 'PreviewController@ajaxPreviewGetStudentCount']);
-    Route::get('ajax-preview-get-student-priority-status', ['as' => 'ajax-preview-get-student-priority-status', 'uses' => 'PreviewController@ajaxPreviewGetStudentPriorityStatus']);
+    Route::post('ajax-preview-get-student-count', ['as' => 'ajax-preview-get-student-count', 'uses' => 'PreviewController@ajaxPreviewGetStudentCount']);
+    Route::post('ajax-preview-get-student-priority-status', ['as' => 'ajax-preview-get-student-priority-status', 'uses' => 'PreviewController@ajaxPreviewGetStudentPriorityStatus']);
     Route::post('ajax-preview-get-student-current-class', ['as' => 'ajax-preview-get-student-current-class', 'uses' => 'PreviewController@ajaxPreviewGetStudentCurrentClass']);
 
     Route::get('ajax-preview-get-remarks', ['as' => 'ajax-preview-get-remarks', 'uses' => 'PreviewController@ajaxPreviewGetRemarks']);
