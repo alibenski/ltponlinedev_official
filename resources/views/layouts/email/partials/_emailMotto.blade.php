14542db66d72223c2f801618d5190f047a3ff7e0
Allyson Frias <allyson.frias@un.org>
2020-05-07 16:16:30 +0200

    create blade template for all emails

---
+++ resources/views/layouts/email/partials/_emailMotto.blade.php
@@ -0,0 +1,12 @@
+<tr>
+    <td bgcolor="#ffffff">
+        <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
+            <tr>
+                <td style="padding: 40px; font-family: sans-serif; font-size: 15px; line-height: 140%; color: #555555;">
+                    <p style="margin: 0;text-align:center;"><strong>The Language Training Programme at the United Nations Office at Geneva</strong></p> 
+                    <p style="margin: 0;text-align: justify;">We believe in multilingualism and multiculturalism as key elements of mutual understanding in a global context. To meet this goal, we offer language courses in the six official languages of the United Nations (Arabic, Chinese, English, French, Russian and Spanish).</p>
+                </td>
+            </tr>
+        </table>
+    </td>
+</tr>
