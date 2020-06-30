b8ce432647a4e945c90d1cccd521cc0275cef289
Allyson Frias <allyson.frias@un.org>
2020-06-30 16:36:58 +0200

    create french translate file for days of the week

---
+++ resources/lang/fr/days.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'Monday' => 'lundi',
+    'Tuesday'     => 'mardi',
+    'Wednesday'     => 'mercredi',
+    'Thursday'     => 'jeudi',
+    'Friday'     => 'vendredi',
+    'Saturday'     => 'samedi',
+    'Sunday'     => 'dimanche',
+];
