1dd7b0eafb22778e9182b705f3f25255c7ca0430
Allyson S. Frias <allyson.frias@gmail.com>
2020-08-24 11:09:09 +0200

    Laravel 5.7 file changes - all database/* files

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('indexno')->nullable();
             $table->string('name');
             $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('approved')->nullable();
             $table->rememberToken();
