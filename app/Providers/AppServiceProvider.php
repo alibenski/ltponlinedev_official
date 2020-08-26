71582ee1644581b34429eef52fb003f21283eccb
Allyson S. Frias <allyson.frias@gmail.com>
2020-08-26 15:56:20 +0200

    delete unused namespaces in AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,8 +8,6 @@ use Illuminate\Support\Facades\Queue;
 use Illuminate\Queue\Events\JobFailed;
 use Validator;
 use App\Preenrolment;
-use App\Term;
-use Carbon\Carbon;
 use Illuminate\Support\Facades\Mail;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,48 +25,48 @@ class AppServiceProvider extends ServiceProvider
             // $event->connectionName
             // $event->job
             // $event->exception
-            Mail::raw("Mail Queue Failing: ".$event->connectionName." ".$event->exception, function($message) {
-            $message->from('clm_language@unog.ch', 'CLM Language');
-            $message->to('allyson.frias@un.org')->subject('Alert: Mail Queue Failing');
+            Mail::raw("Mail Queue Failing: " . $event->connectionName . " " . $event->exception, function ($message) {
+                $message->from('clm_language@unog.ch', 'CLM Language');
+                $message->to('allyson.frias@un.org')->subject('Alert: Mail Queue Failing');
             });
         });
 
-       // Using Closure based composers...
+        // Using Closure based composers...
         view()->composer('partials._nav2', function ($view) {
             $current_enrol_term = \App\Helpers\GlobalFunction::instance()->currentEnrolTermObject();
             $view->with('term', $current_enrol_term);
         });
 
-        Validator::extend('not_equal_to_existing', function($attribute, $value, $parameters, $validator) {
+        Validator::extend('not_equal_to_existing', function ($attribute, $value, $parameters, $validator) {
             // get array values from $validator param
             $data = $validator->getData();
             $staff = $data['INDEXID'];
             $tecode = $data['Te_Code'];
             $form_counter = $data['form_counter'];
             $term = $data['Term'];
-            
+
             $next_term_code = $term;
             $query_form = Preenrolment::orderBy('Term', 'desc')
-                                ->where('INDEXID', $staff)
-                                ->where('Term', $next_term_code)
-                                ->where('Te_Code', $tecode)
-                                ->where('form_counter', $form_counter)
-                                ->first(); 
+                ->where('INDEXID', $staff)
+                ->where('Term', $next_term_code)
+                ->where('Te_Code', $tecode)
+                ->where('form_counter', $form_counter)
+                ->first();
 
             if ($attribute == 'decision') {
                 $existing_appr_value = $query_form->approval;
 
-                if($value != $existing_appr_value){
+                if ($value != $existing_appr_value) {
                     return true;
-                } 
-                    return false;
+                }
+                return false;
             } else if ($attribute == 'decisionhr') {
                 $existing_appr_value_hr = $query_form->approval_hr;
 
-                if($value != $existing_appr_value_hr){
+                if ($value != $existing_appr_value_hr) {
                     return true;
-                } 
-                    return false;
+                }
+                return false;
             }
         });
     }
