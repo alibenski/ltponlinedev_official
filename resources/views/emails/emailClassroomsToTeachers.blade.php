9746d364c65f09ec3bfb99fb69c6ada4b942c843
Allyson S. Frias <allyson.frias@gmail.com>
2020-09-01 10:25:56 +0200

    change title text of classes in email template

--- resources/views/emails/emailClassroomsToTeachers.blade.php
+++ resources/views/emails/emailClassroomsToTeachers.blade.php
@@ -24,7 +24,7 @@
 										<p>{{ $e->Tch_Name }}:</p>
 											@foreach ($e->classrooms as $element)
 											<p style="margin-left: 50px">
-			                            		LTP - {{$element->course->Description}} - {{$element->Te_Code_New}}: {{$element->scheduler->name}} 
+			                            		LTP - {{ $selectedTerm->Term_Code }} - {{ $e->Tch_Lastname }} - {{$element->Te_Code_New}} - {{$element->course->Description}}: {{$element->scheduler->name}} 
 											</p>
 											@endforeach
 										@endif
