1532b255b1330087a29016bc883ba4ad2abf3078
Allyson S. Frias <allyson.frias@gmail.com>
2021-04-07 11:16:40 +0200

    test query testing

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\PlacementForm;
+use App\Preenrolment;
 use Illuminate\Http\Request;
 use App\User;
 use App\Repo;
@@ -9,22 +11,66 @@ use Session;
 
 class TestController extends Controller
 {
-    public function testQuery()
+    public function testQuery($term = "214")
     {
-        $term = '209';
-        $code = 'FAG1-15-209-1';
-        $form = Repo::withTrashed()
-            ->where('CodeClass', $code)
-            ->where('Term', $term)
-            ->join('users', 'ltp_pashqtcur.INDEXID', '=', 'users.indexno')
-            ->orderBy('users.nameLast', 'asc')
-            ->select('ltp_pashqtcur.*')
-            ->get()
-            ->take(10);
+        $pash_records = Repo::where('Term', $term)
+            ->whereHas('classrooms', function($q)
+            {
+                $q->whereNotNull('Tch_ID')
+                    ->where('Tch_ID', '!=', 'TBD');
+            })
+            // ->where('Te_Code', 'like', "%1R%")
+            // ->where(\DB::raw('substr(Te_Code, 2, 2)'), '=' , '1R')
+            // ->whereRaw('SUBSTRING(Te_Code, 2, 2) = "1R"')
+            // ->whereIn('L', ['A','C','R','S'])
+            ->whereHas('courses', function($q2)
+            {
+                $q2->where('level', '1');
+            })
+            ->with('users')->select('INDEXID','Te_Code')->groupBy('INDEXID','Te_Code')->get()->sortBy('Te_Code');
+        $array = [];
+        $arr_exists = [];
+        // dd($pash_records);
+        foreach ($pash_records as $key => $value) {
+            $existing = Repo::where('Term', '<', $term)->where('INDEXID', $value->INDEXID)->exists();
+            // $array[] = $existing;
+            if($existing === false){
+                $array[] = [
+                    'INDEXID' => $value->INDEXID,
+                    'email' => $value->users->email,
+                    'Te_Code' => $value->Te_Code,
+                ];
+            } else {
+                $arr_exists[] = [
+                        'INDEXID' => $value->INDEXID,
+                        'email' => $value->users->email,
+                        'Te_Code' => $value->Te_Code,
+                    ];
+            }
+        }
 
-        dd($form);
+        $fromPlacements = Repo::where('Term', $term)
+            ->whereHas('classrooms', function($q3)
+            {
+                $q3->whereNotNull('Tch_ID')
+                    ->where('Tch_ID', '!=', 'TBD');
+            })
+            ->whereHas('placements', function($query) use ($term){
+                $query->where('Term', $term)->whereIn('L', ['A','C','R','S'])->whereNotNull('CodeIndexID');
+            })
+            // ->whereRaw('SUBSTRING(Te_Code, 2, 2) = "1R"')
+            ->whereIn('L', ['A','C','R','S'])
+            ->with('users')->select('INDEXID','Te_Code')->groupBy('INDEXID','Te_Code')->get()->sortBy('Te_Code');
+        $array2 = [];
+        
+        foreach ($fromPlacements as $key => $value2) {
+            $array2[] = [
+                'email' => $value2->users->email,
+                'Te_Code' => $value2->Te_Code,
+            ];
+        }
 
-        $order = 'asc';
-        $users = User::join('roles', 'users.role_id', '=', 'roles.id')->orderBy('roles.label', $order)->select('users.*')->paginate(10);
+        dd($array, $arr_exists);
+        return $term;
     }
 }
