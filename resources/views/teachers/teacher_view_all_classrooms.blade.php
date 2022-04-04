797fbc356d3810a997fc086c55604b313059c821
Allyson S. Frias <allyson.frias@gmail.com>
2022-04-04 12:24:39 +0200

    Include emailsOneColumnTable for teacher focal points

--- resources/views/teachers/teacher_view_all_classrooms.blade.php
+++ resources/views/teachers/teacher_view_all_classrooms.blade.php
@@ -1,5 +1,6 @@
 @extends('teachers.teacher_template')
 @section('customcss')
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.10.18/af-2.3.3/b-1.5.6/b-colvis-1.5.6/b-flash-1.5.6/b-html5-1.5.6/b-print-1.5.6/cr-1.5.0/fc-3.2.5/fh-3.1.4/kt-2.5.0/r-2.2.2/rg-1.1.0/rr-1.2.4/sc-2.0.0/sl-1.3.0/datatables.min.css"/>
     <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
     <style>
     .close {
@@ -84,6 +85,7 @@
 					<button id="showStudentsBtn" value="{{ $classroom->Code}}" class="btn btn-default btn-space">Show Students</button>
 					<a href="{{ route('teacher-select-week', [$classroom->Code]) }}" class="btn btn-default btn-space">Manage Attendance</a>
 					<button id="enterResultsBtn" value="{{ $classroom->Code}}" class="btn btn-default btn-space">Enter Results</button>
+          <button id="showStudentEmailsBtn" value="{{ $classroom->Code}}" class="btn btn-default btn-space">Show Emails for Moodle</button>
 		      </td>
 		    </tr>
 		    @endforeach
@@ -125,7 +127,8 @@
 
 @section('java_script')
 <script src="{{ asset('js/jquery-2.1.3.min.js') }}"></script>
-
+<script type="text/javascript" src="https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.10.18/af-2.3.3/b-1.5.6/b-colvis-1.5.6/b-flash-1.5.6/b-html5-1.5.6/b-print-1.5.6/cr-1.5.0/fc-3.2.5/fh-3.1.4/kt-2.5.0/r-2.2.2/rg-1.1.0/rr-1.2.4/sc-2.0.0/sl-1.3.0/datatables.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/plug-ins/1.10.19/api/sum().js"></script>
 <script type="text/javascript">
 $(document).ready(function() {
     var token = $("input[name='_token']").val();
@@ -210,6 +213,42 @@ $("button[id='showStudentsBtn']").click(function(){
       }
   });
 }); 
+
+$("button[id='showStudentEmailsBtn']").click(function(){
+  var Code = $(this).val();
+  var token = $("input[name='_token']").val();
+
+  $("button[id='showStudentEmailsBtn'][value='"+Code+"']").addClass('btn-success');
+  $("button[id='showStudentEmailsBtn'][value='"+Code+"']").removeClass('btn-default');
+  $("button").not("button[id='showStudentEmailsBtn'][value='"+Code+"']").addClass('btn-default');
+  $("button").not("button[id='showStudentEmailsBtn'][value='"+Code+"']").removeClass('btn-success');
+
+
+  $.ajax({
+      url: "{{ route('teacher-show-student-emails-only') }}", 
+      method: 'POST',
+      data: {Code:Code, _token:token},
+  })
+  .done(function(data) {
+  		// console.log(data)
+        $(".students-here").html(data);
+        $(".students-here").html(data.options);
+        $('html, body').animate({scrollTop:$(document).height()}, 'slow');
+        
+        if (!$.isArray(data)) {
+            alert("An error occured while loading Show Students Page. Click OK to reload.");
+            window.location.reload();
+        }
+  })
+  .fail(function(data) {
+      console.log("error");
+      alert("An error occured while loading Show Students Page. Click OK to reload.");
+      window.location.reload();
+  })
+  .always(function(data) {
+      console.log("complete show students");
+  });
+});
 
 $("button[id='enterResultsBtn']").click(function(){
   var Code = $(this).val();
