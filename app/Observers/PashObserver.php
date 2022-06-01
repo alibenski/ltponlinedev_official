642e08795a5783f3b41c92a1f0392560d84d462b
Allyson S. Frias <allyson.frias@gmail.com>
2022-06-01 14:58:46 +0200

    modify track history trait to log updates to enrolment and placement models

--- app/Observers/PashObserver.php
+++ app/Observers/PashObserver.php
@@ -19,48 +19,4 @@ class PashObserver
     {
         $this->track($model);
     }
-
-    /**
-     * Handle the repo "created" event.
-     *
-     * @param  \App\Repo  $repo
-     * @return void
-     */
-    public function created(Repo $repo)
-    {
-        //
-    }
-
-    /**
-     * Handle the repo "deleted" event.
-     *
-     * @param  \App\Repo  $repo
-     * @return void
-     */
-    public function deleted(Repo $repo)
-    {
-        //
-    }
-
-    /**
-     * Handle the repo "restored" event.
-     *
-     * @param  \App\Repo  $repo
-     * @return void
-     */
-    public function restored(Repo $repo)
-    {
-        //
-    }
-
-    /**
-     * Handle the repo "force deleted" event.
-     *
-     * @param  \App\Repo  $repo
-     * @return void
-     */
-    public function forceDeleted(Repo $repo)
-    {
-        //
-    }
 }
