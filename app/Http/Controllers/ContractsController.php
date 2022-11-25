ff88c52ec6ccf2b863516b5d91a6e1503043d4e3
Allyson Frias <allyson.frias@un.org>
2022-11-25 14:22:24 +0100

    created function in ContractsController to get contract file path for placement forms; created contract date form for placement form modal;implemented script in selfpay placement forms view that fetches URL and opens window for contract file attachment

--- app/Http/Controllers/ContractsController.php
+++ app/Http/Controllers/ContractsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\ContractFile;
+use App\PlacementForm;
 use App\Preenrolment;
 use Illuminate\Http\Request;
 
@@ -26,5 +27,24 @@ class ContractsController extends Controller
 
             return $data;
         }
+
+        if ($request->formType == 0) {
+            $formId = PlacementForm::orderBy('id', 'desc')->where('INDEXID', $request->indexId)->where('L', $request->L)->where('Term', $request->term)->first();
+            $contractFile = ContractFile::where('user_id', $request->userId)->where('placement_id', $formId->id)->first();
+
+            $data = [
+                'userId' => $request->userId,
+                'L' => $request->L,
+                'term' => $request->term,
+                'path' => $contractFile->path
+            ];
+            if (!$contractFile->path) {
+                $data = 'none';
+            }
+
+            return $data;
+        }
+
+        return "no data";
     }
 }
