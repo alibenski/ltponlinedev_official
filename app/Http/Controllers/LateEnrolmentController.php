4e0ded5c23ab69330549e761407bac2a08b0faa5
Allyson S. Frias <allyson.frias@gmail.com>
2022-12-13 14:49:54 +0100

    In LATE enrolments, show ID and contract attach fields when end of term is gt contract date; ignore validation in controller if no ID and contract files submitted and set ID to NULL

--- app/Http/Controllers/LateEnrolmentController.php
+++ app/Http/Controllers/LateEnrolmentController.php
@@ -573,12 +573,14 @@ class LateEnrolmentController extends Controller
             'payfile' => 'required|mimes:pdf,doc,docx|max:8000',
         ));
 
-        // separated for optional validation in the future
-        $this->validate($request, array(
-            'identityfile' => 'required|mimes:pdf,doc,docx|max:8000',
-            'identityfile2' => 'required|mimes:pdf,doc,docx|max:8000',
-            'contractFile' => 'required|mimes:pdf,doc,docx|max:8000',
-        ));
+        if ($request->has('contractFile')) {
+            // separated for optional validation in the future
+            $this->validate($request, array(
+                'identityfile' => 'required|mimes:pdf,doc,docx|max:8000',
+                'identityfile2' => 'required|mimes:pdf,doc,docx|max:8000',
+                'contractFile' => 'required|mimes:pdf,doc,docx|max:8000',
+            ));
+        }
 
         if ($request->placementDecisionB === '0') {
             $this->validate($request, array(
@@ -628,7 +630,10 @@ class LateEnrolmentController extends Controller
                 'path' => $filestore,
             ]);
             $attachment_identity_file->save();
+        } else {
+            $attachment_identity_file = (object) ['id' => null];
         }
+
         if ($request->hasFile('payfile')) {
             $request->file('payfile');
             $time = date("d-m-Y") . "-" . time();
--- resources/views/form/late/late-selfpay-form.blade.php
+++ resources/views/form/late/late-selfpay-form.blade.php
@@ -106,10 +106,12 @@
                             <strong>Note: accepts pdf, doc, and docx files only. File size must less than 8MB.</strong>
                           </div>
                         
-                          @include('file_attachment_field.id-file-attachment')
+                          @if ($terms->Term_End > $user->contract_date)
+                            @include('file_attachment_field.id-file-attachment')
 
-                          @include('file_attachment_field.contract-file-attachment')
-                          
+                            @include('file_attachment_field.contract-file-attachment')
+                          @endif
+
                           @include('file_attachment_field.payment-file-attachment')
 
                           @include('file_attachment_field.multiple-file-attachment')
