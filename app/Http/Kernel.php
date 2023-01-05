511a36e896f3ec7c410150795bd2b3498d4ea5f5
Allyson Frias <allyson.frias@un.org>
2023-01-05 17:11:51 +0100

    changes made to Middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,7 +14,9 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
+
         \App\Http\Middleware\TrustProxies::class,
+        \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
@@ -82,22 +84,5 @@ class Kernel extends HttpKernel
         'first-time-login' => \App\Http\Middleware\FirstTimeLogin::class,
         'open-close-approval-routes' => \App\Http\Middleware\OpenCloseApprovalRoutes::class,
         'open-close-approval-routes-hr' => \App\Http\Middleware\OpenCloseApprovalRoutesHR::class,
-    ];
-
-    /**
-     * The priority-sorted list of middleware.
-     *
-     * This forces non-global middleware to always be in the given order.
-     *
-     * @var array
-     */
-    protected $middlewarePriority = [
-        \Illuminate\Session\Middleware\StartSession::class,
-        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        \App\Http\Middleware\Authenticate::class,
-        \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        \Illuminate\Session\Middleware\AuthenticateSession::class,
-        \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        \Illuminate\Auth\Middleware\Authorize::class,
     ];
 }
