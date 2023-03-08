8bc4666dc44458c442b450605891bda5628fd3eb
Allyson S. Frias <allyson.frias@gmail.com>
2023-03-08 11:07:56 +0100

    create controller and web route for changing of index numbers

---
+++ app/Http/Controllers/ChangeIndexNumberController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class ChangeIndexNumberController extends Controller
+{
+    public function changeIndexNumberForm()
+    {
+        //
+    }
+}
