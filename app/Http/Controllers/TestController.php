41a2d9055d9ab9110f49034a98c1a2834509cddc
Allyson S. Frias <allyson.frias@gmail.com>
2023-06-28 17:19:01 +0200

    test query modified

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,12 +7,63 @@ use App\Preenrolment;
 use Illuminate\Http\Request;
 use App\User;
 use App\Repo;
+use App\Term;
 use Session;
 
 class TestController extends Controller
 {
     public function testQuery($term = "214")
     {
+        $prev_term = Term::where('Term_Code', 234)->first()->Term_Prev;
+
+        // query students in class
+        $students_in_class = Repo::where('Term', $prev_term)->whereHas('classrooms', function ($query) {
+            $query->whereNotNull('Tch_ID')
+                ->where('Tch_ID', '!=', 'TBD');
+        })
+            ->get();
+        // put inside array
+        $arr1 = [];
+        foreach ($students_in_class as $key1 => $value1) {
+            $arr1[] = $value1->INDEXID;
+        }
+        $arr1 = array_unique($arr1);
+
+        $q = Preenrolment::where('Term', 234)->where('overall_approval', '1')->orderBy('created_at', 'asc')->get();
+        $arr2 = [];
+        foreach ($q as $key2 => $value2) {
+            $arr2[] = $value2->INDEXID;
+        }
+        $arr2 = array_unique($arr2);
+
+        // Compares array1 against one or more other arrays and returns the values in array1 that are not present in any of the other arrays
+        $students_not_in_class = array_diff($arr2, $arr1); // get all enrolment_forms not included in students_in_class
+        $unique_students_not_in_class = array_unique($students_not_in_class);
+
+        $prev_prev_term = Term::where('Term_Code', $prev_term)->first()->Term_Prev;
+
+        $students_within_two_terms = Repo::whereIn('INDEXID', $unique_students_not_in_class)->where('Term', $prev_prev_term)->get();
+        // put inside array
+        $within_two_terms = [];
+        foreach ($students_within_two_terms as $key4 => $value4) {
+            $within_two_terms[] = $value4->INDEXID;
+        }
+        $within_two_terms = array_unique($within_two_terms);
+
+        $students_waitlisted = Repo::where('Term', $prev_term)->whereHas('classrooms', function ($query) {
+            $query->whereNull('Tch_ID')
+                ->orWhere('Tch_ID', '=', 'TBD');
+        })
+            ->get();
+        // put inside array
+        $waitlisted = [];
+        foreach ($students_waitlisted as $key3 => $value3) {
+            $waitlisted[] = $value3->INDEXID;
+        }
+        $waitlisted = array_unique($waitlisted);
+
+        dd($arr1, $arr2, $students_not_in_class, $unique_students_not_in_class, $within_two_terms, $waitlisted);
+
         $pash_records = Repo::where('Term', $term)
             ->whereHas('classrooms', function ($q) {
                 $q->whereNotNull('Tch_ID')
