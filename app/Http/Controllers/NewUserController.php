e2622741ab9655a370940fee6f6def10d9283025
Allyson S. Frias <allyson.frias@gmail.com>
2023-10-24 12:09:54 +0200

    captcha hotfix

--- app/Http/Controllers/NewUserController.php
+++ app/Http/Controllers/NewUserController.php
@@ -228,6 +228,7 @@ class NewUserController extends Controller
 
     public function postNewOutsideUser(Request $request)
     {
+        dd($request->all());
         //validate the data
         $this->validate($request, array(
             'email' => 'required|email',
