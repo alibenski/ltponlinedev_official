2658932dd9222fb38f2f3952f3406ffa9cd2ae2e
Allyson S. Frias <allyson.frias@gmail.com>
2024-01-06 04:47:49 +0100

    viewing all current students table - add attendance fields /admin-student-email-view; create custom attribute/property in Attendance Model and append it to json-type model; AdminController - added id in selected fields; adjust datatables to show  custom attribute: availability

--- app/Attendance.php
+++ app/Attendance.php
@@ -8,8 +8,76 @@ class Attendance extends Model
 {
     protected $table = 'tblLTP_attendance';
 
-    public function attendanceRemarks() {
-        return $this->hasMany('App\AttendanceRemarks', 'attendance_id', 'id'); 
+    public function attendanceRemarks()
+    {
+        return $this->hasMany('App\AttendanceRemarks', 'attendance_id', 'id');
     }
-    
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = ['availability'];
+
+    public function getAvailabilityAttribute()
+    {
+        $qry = Attendance::where('pash_id', $this->pash_id)->get();
+
+        // if no attendance has been entered yet, then 0 value
+        if ($qry->isEmpty()) {
+
+            $data = 0;
+            return response()->json($data);
+        }
+
+        $array_attributes = [];
+        foreach ($qry as $key => $value) {
+            $arr = $value;
+            $array_attributes[] = $arr->getAttributes();
+        }
+
+        $sumP = [];
+        $sumE = [];
+        $sumA = [];
+        $info = [];
+        $collector = [];
+        foreach ($array_attributes as $x => $y) {
+            $info['pash_id'] = $y['pash_id'];
+
+            foreach ($y as $k => $v) {
+                if ($v == 'P') {
+                    $sumP[] = 'P';
+                }
+
+                if ($v == 'E') {
+                    $sumE[] = 'E';
+                }
+
+                if ($v == 'A') {
+                    $sumA[] = 'A';
+                }
+            }
+
+            $info['P'] = count($sumP);
+            $info['E'] = count($sumE);
+            $info['A'] = count($sumA);
+
+            $collector[] = $info;
+            // clear contents of array for the next loop
+            $sumP = [];
+            $sumE = [];
+            $sumA = [];
+        }
+
+        $data = $collector;
+
+        return $collector;
+        // return $this->calculateAvailability();
+    }
+
+    public function calculateAvailability()
+    {
+        return "code here";
+    }
 }
