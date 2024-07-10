6f1892df9faef988017a7bf1e3f33f0655d71827
Allyson S. Frias <allyson.frias@gmail.com>
2024-07-10 16:27:11 +0200

    add teacher_comments to admin assign course view modal

--- resources/views/preenrolment/admin_assign_course.blade.php
+++ resources/views/preenrolment/admin_assign_course.blade.php
@@ -212,6 +212,8 @@
 
 						<textarea id="textarea-{{$element->eform_submit_count}}" name="admin_eform_comment" class="form-control course-no-change" maxlength="3500" @if(is_null($element->admin_eform_comment)) placeholder="Reason for Verify and Not Assigned status, place important information to note about this student, enrolment form, etc." @else placeholder="{{$element->admin_eform_comment}}" @endif></textarea>
 						<br />
+						<label class="control-label">Teacher Comments: <span class="text-danger"></span></label>
+
 						<textarea id="textarea-{{$element->eform_submit_count}}" name="teacher_comments" class="form-control course-no-change" maxlength="3500" readonly @if(is_null($element->teacher_comments)) placeholder="This is a read-only field. No comments from student's teacher." @else placeholder="{{$element->teacher_comments}}"@endif></textarea>
 						
 					</div>
