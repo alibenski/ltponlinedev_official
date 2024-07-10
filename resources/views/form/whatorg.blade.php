d08270a83be72c06dab3125e3f18012f7af1e077
Allyson S. Frias <allyson.frias@gmail.com>
2024-07-10 16:00:49 +0200

    filter profile dropdown due to ILO changes

--- resources/views/form/whatorg.blade.php
+++ resources/views/form/whatorg.blade.php
@@ -190,7 +190,11 @@
           placeholder: "Select Profile",
           });
       } else if ($('#decision2').is(':checked')) {
-        $('#profile option:gt(2)').attr('disabled', 'disabled'); 
+        // $('#profile option:gt(2)').attr('disabled', 'disabled'); 
+        $('#profile option').eq(3).attr('disabled', 'disabled'); 
+        $('#profile option').eq(4).attr('disabled', 'disabled'); 
+        $('#profile option').eq(6).attr('disabled', 'disabled'); 
+        $('#profile option').slice(8).attr('disabled', 'disabled'); 
         $('.select-profile-single').select2({
           placeholder: "Select Profile",
           });
