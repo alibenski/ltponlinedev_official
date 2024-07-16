ba1c8c43dc0528fc38f39fc051939b5f5171cc08
Allyson Frias <allyson.frias@un.org>
2024-07-16 12:42:50 +0200

    put else statement for hard-coded profiles as back-up

--- resources/views/ajax-profile-select.blade.php
+++ resources/views/ajax-profile-select.blade.php
@@ -6,18 +6,19 @@
                 @foreach($profiles as $value)
                 <option value="{{ $value->code }}">{{ $value->name }}</option>
                 @endforeach
+            @else
+                <option value="STF">staff member</option>
+                <option value="INT">intern</option>
+                <option value="CON">consultant</option>
+                <option value="WAE">when actually employed</option>
+                <option value="JPO">JPO</option>
+                {{-- <option value="MSU">staff of a permanent mission</option> --}}
+                <option value="SPOUSE">spouse of UN staff members or spouse of staff of permanent missions</option>
+                <option value="RET">retired UN staff member</option>
+                {{-- <option value="SERV">employee of service providers in the Palais des Nations</option>
+                <option value="NGO">staff of UN-accredited NGO's</option>
+                <option value="PRESS">staff of UN-accredited press corps</option> --}}
             @endif
-          {{-- <option value="STF">staff member</option>
-          <option value="INT">intern</option>
-          <option value="CON">consultant</option>
-          <option value="WAE">when actually employed</option>
-          <option value="JPO">JPO</option>
-          <option value="MSU">staff of a permanent mission</option>
-          <option value="SPOUSE">spouse of UN staff members or spouse of staff of permanent missions</option>
-          <option value="RET">retired UN staff member</option>
-          <option value="SERV">employee of service providers in the Palais des Nations</option>
-          <option value="NGO">staff of UN-accredited NGO's</option>
-          <option value="PRESS">staff of UN-accredited press corps</option> --}}
       </select>
   </div>
 
